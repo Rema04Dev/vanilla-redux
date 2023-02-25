348f3905869c036b7362b7eb89f003f702b9089c
Roman <romansmerdyakov@gmail.com>
2023-02-25 14:15:07 +0600

    init todoReducer

--- src/app.js
+++ src/app.js
@@ -3,4 +3,18 @@ export default () => {
         todos: [],
         currentTask: '',
     }
+
+    const todoReducer = (state = initialState, action) => {
+        switch(action.type) {
+            case 'ADD_TODO': {
+                const { todo } = action.payload;
+                return {
+                    ...state,
+                    todos: [...state.todos, todo],
+                }
+            }
+
+            default: return state;
+        }
+    }
 };
