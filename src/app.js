d537431b7c84e8f3dcd01435f7cfea5295f6c63c
Roman <romansmerdyakov@gmail.com>
2023-02-25 14:34:23 +0600

    realize add todo

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import { createStore } from 'redux';
+import _ from 'lodash';
+
 export default () => {
     const initialState = {
         todos: [],
@@ -8,7 +10,7 @@ export default () => {
     const todoReducer = (state = initialState, action) => {
         switch(action.type) {
             case 'ADD_TODO': {
-                const { todo } = action.payload;
+                const todo = action.payload;
                 return {
                     ...state,
                     todos: [...state.todos, todo],
@@ -20,5 +22,29 @@ export default () => {
     }
 
     const store = createStore(todoReducer);
-    console.log(store.getState());
+
+    const elements = {
+        formTodo: document.querySelector('#todo-form'),
+        inputTodo: document.querySelector('#todo-input'),
+        btnSubmit: document.querySelector('#todo-submit'),
+        listTodo: document.querySelector('#todo-list'),
+    };
+
+    const todoItemEl = `
+    <li class="list-group-item">
+        <button type="button" id="todo-remove" class="btn-close"></button>
+    </li>`;
+
+    elements.formTodo.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        const todoName = new FormData(evt.target).get('todo-name')
+        const formattedTodo = {
+            id: _.uniqueId(),
+            title: todoName.trim(),
+            priority: 'medium' // low, medium, high
+        }
+        store.dispatch({type: 'ADD_TODO', payload: formattedTodo})
+        console.log(store.getState());
+
+    })
 };
